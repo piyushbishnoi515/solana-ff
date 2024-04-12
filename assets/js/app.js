8d35ef98a6fb4ecbbabe0fe05a3045e5b7fddd27
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-04-12 10:43:36 +0530

    full update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,19 @@
-const hidetext = document.querySelector(".hide-text");
-const heading = document.querySelector(".heading");
-const icon = document.querySelector(".icon");
+const accordion = document.querySelectorAll(".accordion-heading");
 
+accordion.forEach(e => {
+    e.addEventListener("click", function () {
+        const acc = document.querySelector(".accordion.active1");
+        e.parentElement.classList.toggle("active1");
+        acc && acc.classList.remove("active1");
+    })
 
-heading.addEventListener("click", function () {
-    hidetext.classList.toggle("active");
-    icon.classList.toggle("iconup");
 })
 
 
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
 
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("showlist");
+})
